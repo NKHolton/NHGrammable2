c24525a731a2260ee651229c858b3bc385f95b02
Natalie Holton <natalie.k.holton@gmail.com>
2016-02-03 18:35:52 +0000

    added test for validation of form

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,12 +8,17 @@ class GramsController < ApplicationController
 
 	def create
 		@gram = Gram.create(gram_params)
-		redirect_to root_path
-	end
+		if @gram.valid?
+    		redirect_to root_path
+  		else
+    		render :new, status: :unprocessable_entity
+    	end
+  	end
 
 	private
 
 	def gram_params
 		params.require(:gram).permit(:message)
 	end
+
 end
